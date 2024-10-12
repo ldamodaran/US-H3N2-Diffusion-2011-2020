264631363c864579bf642323800d16befe13c0d3
ldamodaran <47223725+ldamodaran@users.noreply.github.com>
2024-10-12 18:08:53 -0400

    Add files via upload

--- Phylogenetics/Skyride-plot.R
+++ Phylogenetics/Skyride-plot.R
@@ -1,22 +1,37 @@
-# Lambodhar Damodaran
-# 2021-04-21
-# Script to visualize csv output file(export from Tracer )from BEAST Bayesian Skyride coalescent analysis
-# Input: csv file (remove first line with	GMRF Skyride: xxxxxxxx.log)
-# Output: ggplot of skyride wiht 95% HPD 
-
-
 library(ggplot2)
 library(lubridate)
 
-
-data1 <- read.csv("skyrideouput.csv", header = TRUE)
-data1$date <- as.Date(paste(date_decimal(data1$Time)),"%Y-%m-%d")
-p1 <- ggplot(data1, aes(y=Mean, x=date)) + geom_line() + 
-  geom_ribbon(aes(ymin=Lower, ymax=Upper, alpha = 0.2)) +
-  theme(legend.position = "bottom") + labs(alpha="95% hpd") + 
-  labs(title = "Skyride coalescent") + 
-  scale_y_continuous(name ="Log mean pop (N)", trans = 'log10') 
+setwd("~/skyrides/")
+# read in skyride ouputs from Tracer
+pda <- read.csv("American-2011-2020gmrf.tsv", sep = '\t')
+rand1 <- read.csv("American-2011-2020-rand1.tsv", sep = '\t')
+rand2 <- read.csv("American-2011-2020-rand2.tsv", sep = '\t')
 
 
-p1
+
+combined_sky_data <- rbind(
+  transform(pda, dataset = "pda"),
+  transform(rand1, dataset = "rand1"),
+  transform(rand2, dataset = "rand2")
+)
+
+
+combined_sky_data$date <- as.Date(combined_sky_data$date)
+
+
+skyride_plot <- ggplot(combined_sky_data, aes(x = date, y = mean, color = dataset)) +
+  geom_line(linewidth = 1.5) +
+  geom_ribbon(aes(ymin = lower, ymax = upper, fill = dataset), alpha = 0.3) +
+  labs(title = "Skyride effective population size esitmates A(H3N2)", subtitle = "2011-2020",
+       x = "Date",
+       y = "log(Ne)") +
+  scale_y_log10() +
+  scale_x_date(limits = c(as.Date("2011-05-01"), as.Date("2020-01-01"))) +
+  theme_minimal() +
+  theme(text = element_text(size = 18)) +
+  scale_color_moma_d("VanGogh")
+
+
+skyride_plot
+
 
